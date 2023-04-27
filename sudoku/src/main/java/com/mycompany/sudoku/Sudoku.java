19e3047d561a307a9d3f2dfc0d87f6cce6fe6946
Felipe-Bignoto <felipebignoto@gmail.com>
2023-04-27 08:53:44 -0300

    criando jogo aleatorio

--- sudoku/src/main/java/com/mycompany/sudoku/Sudoku.java
+++ sudoku/src/main/java/com/mycompany/sudoku/Sudoku.java
@@ -4,7 +4,6 @@
 
 package com.mycompany.sudoku;
 import javax.swing.JOptionPane;
-import javax.swing.JCheckBox;
 import java.util.Random;
 /**
  *
@@ -18,24 +17,35 @@ public class Sudoku {
         
         
         int x = menuInicial();
-        if(x == 1){//selecionou o jogo aleatorio
+        if(x == 0){//selecionou o jogo aleatorio
             
             int matr[][] = criaMatriz();
             String quantidadeString = JOptionPane.showInputDialog( "Digite a quantidade de números para serem sorteados: ");
             int quantidade = Integer.parseInt( quantidadeString);
             
-            for(int i =0; i<quantidade; i++){
+            int valor,linha,coluna;
+            for(int i =0; i<quantidade; i++){//ARRUMAR PARA DENTRO DE UM FOR
                 
-                Random aleatorio = new Random();
-                int valor = aleatorio.nextInt(TAMANHO_MAXIMO) + 1;
-                int linha = aleatorio.nextInt(TAMANHO_MAXIMO) + 1;
-                int coluna = aleatorio.nextInt(TAMANHO_MAXIMO) + 1;
-                matr[linha][coluna] = valor;
+                    Random aleatorio = new Random();
+                    valor = aleatorio.nextInt((TAMANHO_MAXIMO - 1) + 1) + 1;
+                    linha = aleatorio.nextInt((TAMANHO_MAXIMO - 1 - 0) + 1) + 0;
+                    coluna = aleatorio.nextInt((TAMANHO_MAXIMO - 1 - 0) + 1) + 0;
+                    System.out.println(linha + " " + coluna + " " + valor);
+                    matr[linha][coluna] = valor;
+                
             }
+            
+            //imprimeJogo(matr);
+            for(int i = 0; i < TAMANHO_MAXIMO; i++){
+            for(int j = 0; j< TAMANHO_MAXIMO; j++){
+                System.out.print(matr[i][j] + " ");
+            }
+            System.out.println();
+        }
         }
         
         else{
-            if(x == 2){//selecionou o definir jogo 
+            if(x == 1){//selecionou o definir jogo 
                 //int matr[][] = criaMatriz();
             }
             else{//não irá iniciar o jogo
@@ -44,6 +54,13 @@ public class Sudoku {
         }
         
     
+    }
+    
+    private static boolean numeroNaPosicao(int [][] matr,int linha, int coluna){
+        if(matr[linha][coluna] != 0){
+            return true;
+        }
+        return false;
     }
     
     private static boolean numeroNaLinha(int [][] matr, int valor, int linha){
@@ -67,9 +84,19 @@ public class Sudoku {
     private static boolean numeroNoBloco(int [][] matr, int valor, int linha, int coluna){
         int inicioBlocoLinha = linha - linha%3;
         int inicioBlocoColuna = coluna - coluna%3;
-        
+        for(int i = inicioBlocoLinha; i < inicioBlocoLinha + 3; i++ ){
+            for(int j = inicioBlocoColuna; j < inicioBlocoColuna +3; j++){
+                if(matr[i][j] == valor){
+                    return true;
+                }
+            }
+        }
         return false;
     }   
+    
+    private static boolean ehValido(int [][] matr, int valor, int linha, int coluna){
+        return !numeroNaLinha(matr, valor, linha) && !numeroNaColuna(matr, valor, coluna) && !numeroNoBloco(matr, valor, linha, coluna) && !numeroNaPosicao(matr, linha, coluna);
+    }
     
     public static int[][] criaMatriz(){
            int [][] matr1 = {
@@ -88,16 +115,17 @@ public class Sudoku {
     }
     
     private static int menuInicial(){
-        JCheckBox check = new JCheckBox("Confirmar");
-        Object[] options = {check,"Jogo aleatório", "Definir jogo"};
-        int x = JOptionPane.showOptionDialog(null, "Confime e selecione uma opção:", "Olá, seja bem vindo ao sudoku!",JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
-        
-        if(check.isSelected()){
-            //System.out.println("Selecionei o " + options[x]);
-            return x;
-        }
-        else{
-            return 0;
+        Object[] options = {"Jogo aleatório", "Definir jogo"};
+        int x = JOptionPane.showOptionDialog(null, "Selecione uma opção:", "Olá, seja bem vindo ao sudoku!",JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        return x;
+    }
+    
+    private static void imprimeJogo(int [][] matr){
+        for(int i = 0; i < TAMANHO_MAXIMO; i++){
+            for(int j = 0; j< TAMANHO_MAXIMO; j++){
+                System.out.print(matr[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
