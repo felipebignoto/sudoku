ea54627700029724f04c11a0c10764881eb8a1bd
Felipe-Bignoto <felipebignoto@gmail.com>
2023-05-07 14:01:59 -0300

    Finalizado

--- sudoku/src/main/java/com/mycompany/sudoku/jogo.java
+++ sudoku/src/main/java/com/mycompany/sudoku/jogo.java
@@ -12,11 +12,11 @@ public class jogo {
 
     public void main(String[] args) {
 
-       criandoJogo();
+        criandoJogo();
     }
-    
+
     private void criandoJogo() {
-        while(REINICIAR == true)  {
+        while (REINICIAR == true) {
             int x = menuInicial();
             if (x == 0 || x == 1) {
                 inicioJogo(x);
@@ -30,18 +30,18 @@ public class jogo {
             JOptionPane.showMessageDialog(null, "Jogo finalizado!");
         }
     }
-    
-    private boolean verificaJogo(){
-        
+
+    private boolean verificaJogo() {
+
         boolean verifica = false;
         boolean semErro = true;
- 
+
         for (int linha = 0; linha < TAMANHO_MAXIMO; linha++) {
             for (int coluna = 0; coluna < TAMANHO_MAXIMO; coluna++) {
-                
-                if(matr[linha][coluna] != 0){
+
+                if (matr[linha][coluna] != 0) {
                     int auxiliar = matr[linha][coluna];
-                    matr[linha][coluna] =0;
+                    matr[linha][coluna] = 0;
                     if (numeroNaPosicao(linha, coluna) == true) {
                         verifica = true;
                         matr[linha][coluna] = auxiliar;
@@ -49,7 +49,7 @@ public class jogo {
                         semErro = false;
                         break;
                     }
-                      
+
                     if (numeroNaLinha(auxiliar, linha)) {
                         verifica = true;
                         matr[linha][coluna] = auxiliar;
@@ -65,7 +65,7 @@ public class jogo {
                         semErro = false;
                         break;
                     }
-                    
+
                     if (numeroNaColuna(auxiliar, coluna)) {
                         verifica = true;
                         matr[linha][coluna] = auxiliar;
@@ -73,23 +73,23 @@ public class jogo {
                         semErro = false;
                         break;
                     }
-                    matr[linha][coluna] =0;
+                    matr[linha][coluna] = 0;
                 }
             }
 
             if (verifica) {
                 break;
             }
-            
+
         }
-        
+
         return semErro;
     }
-    
-    private boolean verificaSeJogoEstaCompleto(){
-        for(int i = 0; i< TAMANHO_MAXIMO; i++){
-            for(int j =0; j< TAMANHO_MAXIMO; j++){
-                if(tabuleiro[i][j] == 0){
+
+    private boolean verificaSeJogoEstaCompleto() {
+        for (int i = 0; i < TAMANHO_MAXIMO; i++) {
+            for (int j = 0; j < TAMANHO_MAXIMO; j++) {
+                if (tabuleiro[i][j] == 0) {
                     return false;
                 }
             }
@@ -125,33 +125,34 @@ public class jogo {
 
                 case 2://verificar
                     //copio o tabuleiro para a matriz
-                    for(int i = 0; i<TAMANHO_MAXIMO;i++){
-                        for(int j =0; j< TAMANHO_MAXIMO;j++){
+                    for (int i = 0; i < TAMANHO_MAXIMO; i++) {
+                        for (int j = 0; j < TAMANHO_MAXIMO; j++) {
                             matr[i][j] = tabuleiro[i][j];
                         }
                     }
                     System.out.println("tabulerio:");
                     imprime(tabuleiro);
                     //verifico se a matr tem solução
-                    if (solucaoSudoku() == true ) {
-                        if (verificaSeJogoEstaCompleto() == true && verificaJogo() == true) {
-                            x = 4;
-                            JOptionPane.showMessageDialog(null, "Parabens, voce ganhou!!!");
-                            //Chamo a nova função
-                            Object[] opcao = {"Inicar novo jogo", "Sair"};
-                            int opcaoEscolhida = JOptionPane.showOptionDialog(null, "*Selecione uma opção:", "Menu!", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcao, opcao[0]);
-                            if(opcaoEscolhida != 0){
-                                REINICIAR = false;
-                                
+                    if (solucaoSudoku() == true) {
+                        if (verificaSeJogoEstaCompleto() == true) {
+                            if (verificaJogo() == true) {
+                                x = 4;
+                                JOptionPane.showMessageDialog(null, "Parabens, voce ganhou!!!");
+                                //Chamo a nova função
+                                Object[] opcao = {"Inicar novo jogo", "Sair"};
+                                int opcaoEscolhida = JOptionPane.showOptionDialog(null, "*Selecione uma opção:", "Menu!", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcao, opcao[0]);
+                                if (opcaoEscolhida != 0) {
+                                    REINICIAR = false;
+
+                                }
                             }
-                        }
-                        else if(verificaJogo() == true){
+
+                        } else if (verificaJogo() == true) {
                             JOptionPane.showMessageDialog(null, "tabuleiro verificado e tem solucao.");
-                        }else{
+                        } else {
                             JOptionPane.showMessageDialog(null, "tabuleiro invalido.");
                         }
-                    }
-                    else{
+                    } else {
                         verificaJogo();
                         JOptionPane.showMessageDialog(null, "tabuleiro verificado e nao tem solucao.");
                     }
@@ -166,7 +167,7 @@ public class jogo {
                     if (x != 3) {
                         JOptionPane.showMessageDialog(null, "Saindo...");
                     }
-                    
+
                     break;
             }
         } while ((x == 0 || x == 1 || x == 2));
@@ -175,7 +176,7 @@ public class jogo {
     private void inicioJogo(int x) {
         if (x == 0) {//selecionou o jogo aleatorio
             criaMatriz(matr);
-            
+
             String quantidadeString = JOptionPane.showInputDialog("Digite a quantidade de números para serem sorteados[0-81]: ");
             try {
                 int quantidade = Integer.parseInt(quantidadeString);
@@ -184,7 +185,7 @@ public class jogo {
                     criaMatriz(tabuleiro);
                     solucaoSudoku();
                     criaTabuleiro(quantidade);
-                    
+
                 } else {
                     JOptionPane.showMessageDialog(null, "Entrada inválida, pois o numero eata fora do intervalo. O jogo sera inicaido com o tabuleiro vazio.");
                 }
@@ -208,70 +209,70 @@ public class jogo {
                         tabuleiro[linha][coluna] = valor;
                     }
                 }
-            }catch(Exception err){
+            } catch (Exception err) {
                 JOptionPane.showMessageDialog(null, "Erro: Entrada inválida, iniciando o jogo vazio.");
             }
         }
     }
-    
-    private void criaTabuleiro(int quantidade){
+
+    private void criaTabuleiro(int quantidade) {
         sorteaTabuleiro(quantidade);
     }
-    
-    private void sorteaTabuleiro(int quantidade){
+
+    private void sorteaTabuleiro(int quantidade) {
         int contador = 0;
-        while(contador < quantidade){
+        while (contador < quantidade) {
             Random aleatorio = new Random();
             int coluna = aleatorio.nextInt((TAMANHO_MAXIMO - 1 - 0) + 1) + 0;
             int linha = aleatorio.nextInt((TAMANHO_MAXIMO - 1 - 0) + 1) + 0;
-            if(tabuleiro[linha][coluna] == 0){
+            if (tabuleiro[linha][coluna] == 0) {
                 contador++;
                 tabuleiro[linha][coluna] = matr[linha][coluna];
             }
         }
     }
-    
-    private boolean solucaoSudoku(){
-        
+
+    private boolean solucaoSudoku() {
+
         boolean verifica = false;
         int linha;
-        int coluna= 0;
+        int coluna = 0;
         for (linha = 0; linha < TAMANHO_MAXIMO; linha++) {
-            for (coluna = 0; coluna < TAMANHO_MAXIMO ; coluna++) {
+            for (coluna = 0; coluna < TAMANHO_MAXIMO; coluna++) {
                 if (matr[linha][coluna] == 0) {
                     verifica = true;
                     break;
                 }
             }
-            if(verifica==true){
-                    break;
+            if (verifica == true) {
+                break;
             }
         }
-        
-        if(verifica == false){
+
+        if (verifica == false) {
             return true;
         }
-        for(int numero = 1; numero <= 9; numero++){
+        for (int numero = 1; numero <= 9; numero++) {
 
-            if(ehValido(numero, linha, coluna)==true){
+            if (ehValido(numero, linha, coluna) == true) {
                 matr[linha][coluna] = numero;
 
-                if(solucaoSudoku()==true){
+                if (solucaoSudoku() == true) {
                     return true;
                 }
             }
-            
+
             matr[linha][coluna] = 0;
         }
         return false;
     }
-    
-    private void preenchDiagonal(){
+
+    private void preenchDiagonal() {
         int indice = 0;
-        while(indice < TAMANHO_MAXIMO){
+        while (indice < TAMANHO_MAXIMO) {
             Random aleatorio = new Random();
             int valor = aleatorio.nextInt((TAMANHO_MAXIMO - 1) + 1) + 1;
-            if(ehValido(valor, indice, indice) == true){
+            if (ehValido(valor, indice, indice) == true) {
                 matr[indice][indice] = valor;
                 indice++;
             }
@@ -328,11 +329,11 @@ public class jogo {
             }
         }
     }
-    
+
     private static int menuInicial() {
         Object[] options = {"Jogo aleatório", "Definir jogo"};
         int x = JOptionPane.showOptionDialog(null, "Selecione uma opção:", "Olá, seja bem vindo ao sudoku!", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
-        if(x != 0 && x!=1) {
+        if (x != 0 && x != 1) {
             REINICIAR = false;
         }
         return x;
@@ -352,6 +353,5 @@ public class jogo {
             System.out.println();
         }
     }
-    
+
 }
- 
